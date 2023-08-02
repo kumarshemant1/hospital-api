1e7bce4946f7912657cbb1363ad3023988dab589
Shemant <kumarshemant1@gmail.com>
2023-08-02 15:19:08 +0530

    Update HospitalApiApplication.java

--- src/main/java/app/hospital/springbootstarter/HospitalApiApplication.java
+++ src/main/java/app/hospital/springbootstarter/HospitalApiApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 @ComponentScan(basePackages = "app.hospital.*") //defined as other components are not placed under 1 package
 public class HospitalApiApplication {
 
 	public static void main(String[] args) {
+		log.info("------------------Success");
 		SpringApplication.run(HospitalApiApplication.class, args);
 
 	}
