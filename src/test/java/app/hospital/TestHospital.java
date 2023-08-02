61b9a0d8e50f93c350e0ce2265c1947463ebafda
Shemant <kumarshemant1@gmail.com>
2023-08-02 15:26:20 +0530

    Create TestHospital.java

---
+++ src/test/java/app/hospital/TestHospital.java
@@ -0,0 +1,13 @@
+package app.hospital;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestHospital {
+
+	@Test
+	public void test() {
+		assertTrue(Boolean.TRUE);
+	}
+}
